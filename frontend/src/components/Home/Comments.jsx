34622ecda5d4ffe2d54f294f68f696979edad977
YazanAlshoubakii <yazan.h.alshoubaki@gmail.com>
2024-06-23 03:56:59 +0300

    modify comments component to render it in a modal

---
+++ frontend/src/components/Home/Comments.jsx
@@ -0,0 +1,66 @@
+import { useContext, useState } from 'react';
+import { UserData } from '../../App';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Button from 'react-bootstrap/esm/Button';
+import axios from 'axios';
+
+const Comments = ({ service, close, services }) => {
+  const { token, userId } = useContext(UserData);
+  const [newComment, setNewComment] = useState('');
+
+  const serviceId = service._id;
+
+  const addComment = (serviceId) => {
+    const comment = {
+      comment: newComment,
+      commenter: userId,
+    };
+    axios
+      .post(`http://localhost:5000/services/${serviceId}/comments`, comment, {
+        headers: { authorization: token },
+      })
+      .then((res) => {
+        console.log(res);
+        close();
+        services();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  return (
+    <div>
+      <InputGroup className="mb-3">
+        <Form.Control
+          aria-label="Text input with button"
+          placeholder="Enter Comment"
+          onChange={(e) => {
+            setNewComment(e.target.value);
+          }}
+        />
+      </InputGroup>
+      <Button
+        style={{ width: '100%' }}
+        onClick={() => {
+          addComment(serviceId);
+        }}
+      >
+        Comment
+      </Button>
+      {service.comments.map((x) => {
+        return (
+          <div key={x._id}>
+            <p>
+              {x.commenter.userName} : {x.comment}
+            </p>
+            <hr />
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Comments;
--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -4,16 +4,23 @@ import { useNavigate } from 'react-router-dom';
 import { UserData } from '../../App';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import Accordion from 'react-bootstrap/Accordion';
-import Form from 'react-bootstrap/Form';
-import InputGroup from 'react-bootstrap/InputGroup';
+import Modal from 'react-bootstrap/Modal';
+import Comments from './Comments';
 import './style.css';
 
 const Home = () => {
   const navigator = useNavigate();
-  const { token, userId, setUserId, isLoggedIn } = useContext(UserData);
+  const { token, setUserId, isLoggedIn } = useContext(UserData);
   const [services, setServices] = useState([]);
-  const [newComment, setNewComment] = useState('');
+  const [showComments, setShowComments] = useState(false);
+  const [service, setService] = useState(null);
+
+  const handelCloseComments = () => {
+    setShowComments(false);
+  };
+  const handelShowComments = () => {
+    setShowComments(true);
+  };
 
   const getServices = () => {
     axios
@@ -26,24 +33,6 @@ const Home = () => {
       })
       .catch(() => {
         navigator('/login');
-      });
-  };
-
-  const addComment = (serviceId) => {
-    const comment = {
-      comment: newComment,
-      commenter: userId,
-    };
-    axios
-      .post(`http://localhost:5000/services/${serviceId}/comments`, comment, {
-        headers: { authorization: token },
-      })
-      .then((res) => {
-        console.log(res);
-        getServices();
-      })
-      .catch((err) => {
-        console.log(err);
       });
   };
 
@@ -67,50 +56,42 @@ const Home = () => {
                 <p>Price: {e.price}</p>
                 <p>Estimated Time: {e.estimatedTime}</p>
                 <p>Service Provider: {e.serviceProvider.userName}</p>
-                <Button variant="primary">Order</Button>
+                <Button
+                  variant="secondary"
+                  onClick={() => {
+                    setService(e);
+                    handelShowComments();
+                  }}
+                >
+                  Comments
+                </Button>
               </Card.Body>
               <Card.Body>
-                <Accordion>
-                  <Accordion.Item eventKey="0">
-                    <Accordion.Header>Comments</Accordion.Header>
-                    <Accordion.Body>
-                      <InputGroup className="mb-3">
-                        <Form.Control
-                          aria-label="Text input with button"
-                          placeholder="Enter Comment"
-                          onChange={(e) => {
-                            setNewComment(e.target.value);
-                          }}
-                        />
-                      </InputGroup>
-                      <Button
-                        style={{ width: '100%' }}
-                        onClick={() => {
-                          addComment(e._id);
-                        }}
-                      >
-                        Comment
-                      </Button>
-
-                      {/* {console.log(comments)} */}
-                      {e.comments.map((x) => {
-                        return (
-                          <div key={x._id}>
-                            <p>
-                              {x.commenter.userName} : {x.comment}
-                            </p>
-                            <hr />
-                          </div>
-                        );
-                      })}
-                    </Accordion.Body>
-                  </Accordion.Item>
-                </Accordion>
+                <Button variant="primary" style={{ width: '100%' }}>
+                  Order
+                </Button>
               </Card.Body>
             </Card>
           );
         })}
       </div>
+      <Modal show={showComments} onHide={handelCloseComments}>
+        <Modal.Header closeButton>
+          <Modal.Title>Comments</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Comments
+            service={service}
+            services={getServices}
+            close={handelCloseComments}
+          />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handelCloseComments}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 };
